2ba9a40d884c865c5e22a2c7ecea7fcc9bd8c158
Mariusz Dzikowski <thecyste@gmail.com>
2024-04-17 13:46:41 +0200

    Remove unused config variables.

--- src/config.ts
+++ src/config.ts
@@ -9,11 +9,7 @@ export const DB_NAME = process.env.DB_NAME ?? 'WildDrop'
 export const DB_USER = process.env.DB_USER ?? 'WildDrop'
 export const DB_PASS = process.env.DB_PASS ?? 'WildDrop'
 export const DB_DEBUG = s2b(process.env.DB_PASS)
-export const REDIS_HOST = process.env.REDIS_HOST ?? 'localhost'
 
-export const SESSION_PASSPHRASE = process.env.SESSION_PASSPHRASE ?? 'passphrase'
-export const SESSION_PRIVATE_KEY = process.env.SESSION_PRIVATE_KEY ?? 'sec/private.key'
-export const SESSION_PUBLIC_KEY = process.env.SESSION_PUBLIC_KEY ?? 'sec/public.key'
 export const SESSION_EXPIRE = Number(process.env.SESSION_EXPIRE) || 60
 
 export const AZ_CONTRACT = process.env.AZ_CONTRACT
